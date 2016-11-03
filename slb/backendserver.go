63dd32e58a42e207bfd6e96678eba6fab7f87457
xiqiang <xiqiang@staff.weibo.com>
2016-11-03 15:52:43 +0800

    support slb vservergroup

--- slb/backendserver.go
+++ slb/backendserver.go
@@ -7,6 +7,9 @@ type BackendServerService interface {
 	RemoveBackendServers(params map[string]interface{}) (RemoveBackendServersResponse, error)
 	SetBackendServers(params map[string]interface{}) (SetBackendServersResponse, error)
 	DescribeHealthStatus(params map[string]interface{}) (DescribeHealthStatusResponse, error)
+	AddVServerGroupBackendServers(params map[string]interface{}) (VGroup_AddBackendServersResponse, error)
+	RemoveVServerGroupBackendServers(params map[string]interface{}) (VGroup_RemoveBackendServersResponse, error)
+	DescribeVServerGroupAttribute(params map[string]interface{}) (VGroup_DescribeHealthStatusResponse, error)
 }
 
 type BackendServerOperator struct {
@@ -50,6 +53,38 @@ type DescribeHealthStatusResponse struct {
 	BackendServers BackendServerList `json:"BackendServers"`
 }
 
+type VGroup_BackendServerType struct {
+	ServerId           string `json:"ServerId"`
+	Port             int    `json:"Port"`
+	Weight             int    `json:"Weight"`
+}
+
+type VGroup_BackendServerList struct {
+	VGroup_BackendServer []VGroup_BackendServerType `json:"BackendServer"`
+}
+
+// Response struct for AddBackendServers
+type VGroup_AddBackendServersResponse struct {
+	util.ErrorResponse
+	VServerGroupId string            `json:"VServerGroupId"`
+	VGroup_BackendServers VGroup_BackendServerList `json:"BackendServers"`
+}
+
+// Response struct for RemoveBackendServers
+type VGroup_RemoveBackendServersResponse struct {
+	util.ErrorResponse
+	VServerGroupId string            `json:"VServerGroupId"`
+	VGroup_BackendServers VGroup_BackendServerList `json:"BackendServers"`
+}
+
+// Response struct for DescribeHealthStatus
+type VGroup_DescribeHealthStatusResponse struct {
+	util.ErrorResponse
+	VServerGroupId string            `json:"VServerGroupId"`
+	VServerGroupName string            `json:"VServerGroupName"`
+	VGroup_BackendServers VGroup_BackendServerList `json:"BackendServers"`
+}
+
 func (op *BackendServerOperator) AddBackendServers(params map[string]interface{}) (AddBackendServersResponse, error) {
 	var resp AddBackendServersResponse
 	err := op.Common.Request(util.GetFuncName(1), params, &resp)
@@ -73,3 +108,22 @@ func (op *BackendServerOperator) DescribeHealthStatus(params map[string]interfac
 	err := op.Common.Request(util.GetFuncName(1), params, &resp)
 	return resp, err
 }
+
+func (op *BackendServerOperator) AddVServerGroupBackendServers(params map[string]interface{}) (VGroup_AddBackendServersResponse, error) {
+	var resp VGroup_AddBackendServersResponse
+	err := op.Common.Request(util.GetFuncName(1), params, &resp)
+	return resp, err
+}
+
+func (op *BackendServerOperator) RemoveVServerGroupBackendServers(params map[string]interface{}) (VGroup_RemoveBackendServersResponse, error) {
+	var resp VGroup_RemoveBackendServersResponse
+	err := op.Common.Request(util.GetFuncName(1), params, &resp)
+	return resp, err
+}
+
+func (op *BackendServerOperator) DescribeVServerGroupAttribute(params map[string]interface{}) (VGroup_DescribeHealthStatusResponse, error) {
+	var resp VGroup_DescribeHealthStatusResponse
+	err := op.Common.Request(util.GetFuncName(1), params, &resp)
+	return resp, err
+}
+
